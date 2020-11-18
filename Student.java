efc395d329ec74a020b30ddbd2a8acb16a421163
smuraweh <smuraweh@asu.edu>
2020-11-17 19:17:05 -0700

    Initial commit

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private int id;
+    private string firstName;
+    private string lastName;
+    private string program;
+    private string level;
+    private string asurite;
+
+    public Student(){
+        id = null;
+        firstName = null;
+        lastName = null;
+        program = null;
+        level = null;
+        asurite = null;
+    }
+
+    public void setFirstName(string firstName) {
+        this.firstName = firstName;
+    }
+}
