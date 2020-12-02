09776ae320b3d5d7c5f8d64960a781ea7c4aeb3e
mechrodancer <stickman331@gmail.com>
2020-12-01 17:26:07 -0700

    Update date.java

--- date.java
+++ date.java
@@ -1,16 +1,72 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 public class date extends javax.swing.JFrame {
 
 
+	 public static <T> ArrayList<T> removeDuplicates(ArrayList<T> list) 
+	    { 
+	  
+	        // Create a new ArrayList 
+	        ArrayList<T> newList = new ArrayList<T>(); 
+	  
+	        // Traverse through the first list 
+	        for (T element : list) { 
+	  
+	            // If this element is not present in newList 
+	            // then add it 
+	            if (!newList.contains(element)) { 
+	  
+	                newList.add(element); 
+	            } 
+	        } 
+	  
+	        // return the new list 
+	        return newList; 
+	    } 
+	
+	
     public date() {
     	
         initComponents();       
     }
+    public static <T> List<T> convert(List<T> newList) 
+    { 
+  
+        List<T> ASURITE = new LinkedList<>(); 
+        for (T t : newList) { 
 
+            ASURITE.add(t); 
+        } 
+
+        return ASURITE; 
+    }
+    public static <T> List<T> converttimes(List<T> newTime) 
+    { 
+  
+        List<T> ATIME = new LinkedList<>(); 
+        for (T t : newTime) { 
+
+            ATIME.add(t); 
+        } 
+
+        return ATIME; 
+    }
+  
 
     @SuppressWarnings("unchecked")                        
     private void initComponents() {
-
+        
         jLabel1 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         jComboBox2 = new javax.swing.JComboBox<>();
@@ -18,7 +74,8 @@ public class date extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-
+        
+     
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
@@ -106,8 +163,111 @@ public class date extends javax.swing.JFrame {
        
         String date = (month+ " "+day);
         System.out.println(date);
-     
-        System.exit(0);
+      
+        
+        
+   	 ArrayList<Double> TIME = new ArrayList<>();
+		
+		//Hashtable<String, String> attend = new Hashtable<>(); 
+		ArrayList<String> ASUR = new ArrayList<>();
+		
+
+		
+		JButton open = new JButton();
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new java.io.File("Desktop"));
+		fc.setDialogTitle("Select CSV FILE");
+	//	fc.setFileSelectionMode("JFileChooser.") possibly make only csv files selectable
+		if (fc.showOpenDialog(open) == JFileChooser.APPROVE_OPTION)
+		{
+			//stuff if approved file
+		}
+		else if (fc.showOpenDialog(open) == JFileChooser.CANCEL_OPTION)
+		{
+			//stuff if approved file
+		}
+		//System.out.println(fc.getSelectedFile().getAbsolutePath());
+		
+		String path = fc.getSelectedFile().getAbsolutePath();
+		 String line = "";
+		 try {
+		 BufferedReader reader = new BufferedReader(new FileReader(path));
+		 
+		 while((line = reader.readLine()) != null) {
+			 String[] stuff = line.split(",");
+			ASUR.add(stuff[0]);
+		
+			double dnum = Double.parseDouble(stuff[1]);
+			TIME.add(dnum);
+			
+		
+		 }
+		 
+		 ArrayList<String> newList = removeDuplicates(ASUR);
+		 ArrayList<Double> newTIME = new ArrayList<>();
+		 int size =  newList.size();
+		 int size2 =  ASUR.size();
+		 int k = 0;
+		 double g = 0;
+		 int i = 0;
+		 int q= 0;
+		 
+		while(i < size)
+		{
+			
+			
+			q=0;
+			 k = 0;
+			 g = 0; 
+		 while ( k < size2)
+		 {
+			 boolean isEqual = ASUR.get(k).equals(newList.get(i)); 
+			 
+			 if( q == 0)
+			 {
+				g = g + TIME.get(k);
+				
+				 newTIME.add(g);
+			//	 System.out.println(k);
+				 k++;
+			//	 System.out.println(g);
+				 q++;
+				 if(i >0)
+				 {
+					 g = g - TIME.get(0);
+					 newTIME.set(i, g);
+				 }
+				 
+			 }
+			 
+			 else if(isEqual)
+			 {
+				 g = g + TIME.get(k);
+				 newTIME.set(i, g);
+				 k++;
+				 
+			 }
+			
+			 else 
+			 {
+				 k++;
+			 }
+			
+		 }
+		 i++;
+		}
+		
+		List<String> ASURITE = convert(newList); 
+		List<Double> ATIME = converttimes(newTIME); 
+		System.out.println(ASURITE);
+		System.out.println(ATIME);
+		 
+		 }catch(FileNotFoundException a) {
+			 System.out.println("AAAAAAA");
+		 }catch(IOException a) {
+			 System.out.println("AAAAAAAX2");
+		 }
+       System.exit(0);
     }                                        
 
    
