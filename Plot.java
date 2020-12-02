52b53d7256ee8a5b8d5b92a3f1819001ff33db70
JRTSumner2000 <44513411+JRTSumner2000@users.noreply.github.com>
2020-12-01 19:39:31 -0700

    Update Plot.java

--- Plot.java
+++ Plot.java
@@ -7,7 +7,10 @@ import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
@@ -16,9 +19,9 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class Plot extends JFrame {
 	
 	Student student = new Student();
-	Add attendance = new Add();
+	//Add attendance = new Add();
 	
-	public Plot(LinkedList<Student> studentList)
+	public Plot(Student[] studentList)
 	{	
 		String chartTitle = "Attendance";
 	    String xAxisLabel = "ID Number";
@@ -27,33 +30,43 @@ public class Plot extends JFrame {
 	    XYSeriesCollection data = new XYSeriesCollection();
 	    XYSeries series1 = new XYSeries("Attendance 1");
 	 
-	    /*series1.add(1.0, 2.0);
-	    series1.add(2.0, 3.0);
-	    series1.add(3.0, 2.5);
-	    series1.add(3.5, 2.8);
-	    series1.add(4.2, 6.0);*/
+	    /*series1.add(1.0, 0.3);
+	    series1.add(2.0, 0.3);
+	    series1.add(3.0, 0.25);
+	    series1.add(3.5, 0.35);
+	    series1.add(4.2, 0.60);*/
 	    
 	    data.addSeries(series1);
 	    
 	    XYDataset dataset = data;
 	    
-	    for(int i = 0; i < studentList.size(); i++)
+	    for(int i = 0; i < studentList.length; i++)
 	    {
-	    	double idNum = Double.parseDouble(studentList.get(i).getID());
-	    	double minutes = Double.parseDouble(attendance); // For attendance
+	    	double idNum = Double.parseDouble(studentList[i].getID());
+	    	double minutes = studentList[i].attendance.attendanceAmount; // For attendance
 	    	series1.add(idNum, minutes);
 	    }
 		 
 	    JFreeChart chart = ChartFactory.createScatterPlot(chartTitle, xAxisLabel, yAxisLabel, dataset);
 		
-		JPanel chartPanel = new ChartPanel(chart);
+		XYPlot xyPlot = (XYPlot) chart.getPlot();
+        xyPlot.setRangeCrosshairVisible(true);
+        XYItemRenderer renderer = xyPlot.getRenderer();
+        renderer.setSeriesPaint(0, Color.blue);
+        NumberAxis domain = (NumberAxis) xyPlot.getDomainAxis();
+        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
+        range.setRange(0.0, 1.0);
+        range.setTickUnit(new NumberTickUnit(0.05));
+        
+        JPanel chartPanel = new ChartPanel(chart);
+        
 		//JFrame frame = new JFrame();
         this.add(chartPanel, BorderLayout.CENTER);
         this.setSize(1920, 1080);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public void display(LinkedList<Student> studentList)
+	public void display(Student[] studentList)
 	{
 		new Plot(studentList).setVisible(true);
 	}
