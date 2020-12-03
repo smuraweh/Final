6ba9cb2f4d7362ee4ba14e2a43e1001c7b80012d
JRTSumner2000 <44513411+JRTSumner2000@users.noreply.github.com>
2020-12-02 22:36:01 -0700

    Update Plot.java

--- Plot.java
+++ Plot.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.LinkedList;
+import java.util.Random;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -20,11 +21,21 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+/**
+ * 
+ * @author JRTSN
+ * Extends JFrame to display the data and uses JFreeChart as an external library
+ */
 public class Plot extends JFrame {
 	
 	Student student = new Student();
 	//Add attendance = new Add();
 	
+	/**
+	 * 
+	 * @param table
+	 * Creates the plot chart and finds all the data
+	 */
 	public Plot(JTable table)
 	{	
 		DefaultTableModel model = (DefaultTableModel)table.getModel();
@@ -39,11 +50,13 @@ public class Plot extends JFrame {
 	    XYSeriesCollection data = new XYSeriesCollection();
 	    XYSeries series1 = new XYSeries("Attendance 1");
 	 
-	    /*series1.add(1.0, 0.3);
-	    series1.add(2.0, 0.3);
-	    series1.add(3.0, 0.25);
-	    series1.add(3.5, 0.35);
-	    series1.add(4.2, 0.60);*/
+	    /*
+	     * Test Data:
+	     * series1.add(1.0, 0.3);
+	     * series1.add(2.0, 0.3);
+	     * series1.add(3.0, 0.25);
+	     * series1.add(3.5, 0.35);
+	     * series1.add(4.2, 0.60);*/
 	    
 	    String date = null;
 	    LinkedList<String> dates = new LinkedList();
@@ -51,8 +64,14 @@ public class Plot extends JFrame {
 	    int totalDates = numColumns - 6;
 	    XYDataset dataset = data;
 	    
+	    /*
+	     * Moves through the rows
+	     */
 	    for(int i = 0; i < numRows; i++)
 	    {
+	    	/*
+	    	 * Moves through the columns
+	    	 */
 	    	for(int j = 6; j < numColumns; j++)
 	    	{
 	    		double id = (double) model.getValueAt(i, 1);
@@ -71,16 +90,26 @@ public class Plot extends JFrame {
         xyPlot.setRangeCrosshairVisible(true);
         LegendItemCollection chartLegend = new LegendItemCollection();
         Shape shape = new Rectangle(10, 10);
+        /*
+         * Sets the legend
+         */
+
+        Random rand = new Random();
         for(int i = 0; i < totalDates; i++)
         {
-        	chartLegend.add(new LegendItem(dates.get(i), null, null, null, shape, Color.blue));
+        	float r = rand.nextFloat();
+            float g = rand.nextFloat();
+            float b = rand.nextFloat();
+
+            Color randomColor = new Color(r, g, b);
+        	chartLegend.add(new LegendItem(dates.get(i), null, null, null, shape, randomColor));
         }
         xyPlot.setFixedLegendItems(chartLegend);
         XYItemRenderer renderer = xyPlot.getRenderer();
         renderer.setSeriesPaint(0, Color.blue);
-        NumberAxis domain = (NumberAxis) xyPlot.getDomainAxis();
-        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
-        range.setRange(0, 100);
+        NumberAxis domain = (NumberAxis) xyPlot.getDomainAxis(); //Sets xAxis
+        NumberAxis range = (NumberAxis) xyPlot.getRangeAxis(); //Sets yAxis
+        range.setRange(0, 100); //YAxis is 0 to 100
         range.setTickUnit(new NumberTickUnit(5));
         
         JPanel chartPanel = new ChartPanel(chart);
@@ -91,6 +120,11 @@ public class Plot extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	/**
+	 * 
+	 * @param table
+	 * Method called in order to display the table
+	 */
 	public void display(JTable table)
 	{
 		new Plot(table).setVisible(true);
